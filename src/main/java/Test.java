45fcd33b1ef22d2afea9a8cc16fa9605f3d3cf74
dmitriy.mukhin <dmitriy.mukhin@gmail.com>
2016-05-11 16:32:40 -0400

    more stream examples

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,12 @@
 
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
 //import java.util.List;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 
 //import org.springframework.util.CollectionUtils;
 
@@ -42,6 +46,37 @@ public class Test {
 		System.out.println(Arrays.toString(b) + " and " + Arrays.toString(c) + (intersect(b,c)?"":" do not") + " intersect");
 		b=new int[]{1,5}; c=new int[]{8,0};
 		System.out.println(Arrays.toString(b) + " and " + Arrays.toString(c) + (intersect(b,c)?"":" do not") + " intersect");
+
+		class NV {public String name, value;@Override public String toString(){return name+":"+value;}}
+		List<NV> l = Arrays.asList(
+				new NV() {{name="a";value="1";}},
+				new NV() {{name="k";value="15";}},
+				new NV() {{name="c";value="3";}},
+				new NV() {{name="b";value="2";}});
+		
+		System.out.println(l.stream()
+				.sorted((o1,o2) -> Integer.parseInt(o1.value)-Integer.parseInt(o2.value))
+				.map(Object::toString)
+//				.collect(Collectors.joining(", ", "[", "]")));
+				.collect(StringBuilder::new,
+						(_a,_s)->{if(_a.length()>0){_a.append(", ").append(_s);}else{_a.append(_s);}},
+//						new BiConsumer<StringBuilder, String>() {
+//							@Override
+//							public void accept(StringBuilder t, String u) {
+//								StringBuilder tmp = t.length()>0?t.append(", ").append(u):t.append(u);
+//							}
+//						},
+						StringBuilder::append)
+				.insert(0, "[").append("]"));
+		
+		l.sort(new Comparator<NV>() {
+			@Override
+			public int compare(NV o1, NV o2) {
+				return Integer.parseInt(o1.value) - Integer.parseInt(o2.value);
+			}
+		});
+
+		System.out.println(Arrays.toString(l.toArray()));
 	}
 	
 
